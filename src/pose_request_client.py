5c371551673694a04361fa35588bcc273b516691
Daniel Ingram <ingramds@appstate.edu>
2018-03-08 23:31:22 -0500

    Implemented hard-coded pick and place

--- src/pose_request_client.py
+++ src/pose_request_client.py
@@ -3,26 +3,41 @@ import rospy
 from roboarm.srv import PoseRequest, PoseRequestResponse
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Pose
+from tf.transformations import quaternion_from_euler
+from math import pi
 
+def set_pose(pose, x, y, z, roll, pitch, yaw):
+	q = quaternion_from_euler(roll, pitch, yaw)
+	pose.orientation.x = q[0]
+	pose.orientation.y = q[1]
+	pose.orientation.z = q[2]
+	pose.orientation.w = q[3]
+	pose.position.x = x
+	pose.position.y = y
+	pose.position.z = z
 
 rospy.init_node('pose_request_client', anonymous=True)
 rospy.wait_for_service('goto_pose_server')
 
 goto_pose = rospy.ServiceProxy('goto_pose_server', PoseRequest)
-gripper_pub = rospy.Publisher('/roboarm/gripper_controller/command', Float64, queue_size=10)
+gripper_pub = rospy.Publisher('/roboarm/gripper_controller/command', Float64, queue_size=10, latch=True)
 
 gripper_msg = Float64()
 gripper_msg.data = 0.3
 
 pose = Pose()
-pose.orientation.x = -0.501634507333
-pose.orientation.y = 1.72876577259e-05
-pose.orientation.z = -4.46482869593e-05
-pose.orientation.w = 0.865079660355
-pose.position.x = 1.06955479166
-pose.position.y = 0.775824538778
-pose.position.z = 1.42802207074
+set_pose(pose, 1.07, 0.78, 0.25, pi, 0, 0)
+response = goto_pose(pose)
+if response.result:
+	gripper_pub.publish(gripper_msg)
 
-goto_pose(pose)
+rospy.sleep(1)
 
-gripper_pub.publish(gripper_msg, latch=True)
+set_pose(pose, 0, 1, 1.2, pi, 0, 0)
+response = goto_pose(pose)
+
+set_pose(pose, -1, 1, 1.1, pi, 0, 0)
+response = goto_pose(pose)
+if response.result:
+	gripper_msg.data = 0
+	gripper_pub.publish(gripper_msg)
